a1cc43e35011999737807a93548d98c6ed5c8477
Mateusz Parzonka <parzonka@gmail.com>
2012-11-26 17:02:28 +0100

    Remove 'converge' option from 'runBenchmark.sh'

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -9,4 +9,4 @@
 # list of all benchmarks can be found at http://dacapobench.org/benchmarks.html 
 
 # load-time weaving via aspectjweaver 1.6.12
-java -Xms256M -Xmx1024M -jar lib/dacapo-9.12-bach.jar $1 --converge
+java -Xms256M -Xmx1024M -jar lib/dacapo-9.12-bach.jar $1
