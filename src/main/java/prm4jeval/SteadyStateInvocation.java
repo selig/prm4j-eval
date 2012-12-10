1eb59d8935fe189b2cba5b6c9effe7e813db5c01
Mateusz Parzonka <parzonka@gmail.com>
2012-12-10 08:29:31 +0100

    Add SteadyStateInvocation

---
+++ src/main/java/prm4jeval/SteadyStateInvocation.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4jeval;
+
+import java.io.Serializable;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class SteadyStateInvocation implements Serializable {
+
+    private static final long serialVersionUID = -5341292262089739659L;
+
+    /**
+     * When the coefficient of standard deviation falls below this value, we have entered steady state.
+     */
+    protected final double covThreshold;
+    protected final int window;
+    private final DescriptiveStatistics measurements;
+
+    public SteadyStateInvocation(int window, double covThreshold) {
+	this.window = window;
+	this.covThreshold = covThreshold;
+	measurements = new DescriptiveStatistics(window);
+    }
+
+    public SteadyStateInvocation() {
+	window = Integer.parseInt(getSystemProperty("prm4jeval.window", "5"));
+	covThreshold = Double.parseDouble(getSystemProperty("prm4jeval.covThreshold", "0.02"));
+	measurements = new DescriptiveStatistics(window);
+    }
+
+    /**
+     *
+     * @param time
+     * @return <code>true</code> if more measurements are needed
+     */
+    public boolean addMeasurement(long time) {
+	final double value = new Long(time).doubleValue();
+	measurements.addValue(value);
+	return isThresholdReached();
+    }
+
+    public boolean isThresholdReached() {
+	if (measurements.getN() < window) {
+	    return false;
+	}
+	double standardDeviation = measurements.getStandardDeviation();
+	double mean = measurements.getMean();
+	double coefficientOfStandardDeviation = standardDeviation / mean;
+	System.out.println("Coefficient of standard deviation is: " + coefficientOfStandardDeviation);
+	if (coefficientOfStandardDeviation < covThreshold) {
+	    System.out.println("Reached");
+	    return true;
+	}
+	return false;
+    }
+
+    /**
+     * @return the mean of the measurements
+     */
+    public double getMean() {
+	return measurements.getMean();
+    }
+
+    /**
+     * @return the measurementCount
+     */
+    public long getMeasurementCount() {
+	return measurements.getN();
+    }
+
+    static String getSystemProperty(String key, String defaultValue) {
+	final String value = System.getProperty(key);
+	return value != null ? value : defaultValue;
+    }
+
+}
