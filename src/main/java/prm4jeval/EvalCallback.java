5196893642d79fc7d9d684ba4f513e7cef0304d4
Mateusz Parzonka <parzonka@gmail.com>
2012-12-10 12:10:59 +0100

    Use EvaluationData in EvalCallback
    Add SteadyStateEvaluation

--- src/main/java/prm4jeval/EvalCallback.java
+++ src/main/java/prm4jeval/EvalCallback.java
@@ -1,21 +1,31 @@
 package prm4jeval;
+
+import java.util.Arrays;
+
 import org.dacapo.harness.Callback;
 import org.dacapo.harness.CommandLineArgs;
 import org.dacapo.parser.Config;
 
 public class EvalCallback extends Callback {
 
+    private final EvaluationData evaluationData;
+    private final SteadyStateEvaluation sse;
+
     private int iterationCount = 0;
-    private final int maxIterations = 5;
+
+    private long startTime;
 
     public EvalCallback(CommandLineArgs args) {
 	super(args);
-	System.out.println("EvalCallback loaded.");
+	evaluationData = new EvaluationData("evaluationData.ser");
+	sse = evaluationData.getSteadyStateEvalation(System.getProperty("prm4jeval.benchmark"),
+		System.getProperty("prm4jeval.parametricProperty"));
+	System.out.println("EvalCallback loaded." + Arrays.toString(args.getArgs()));
     }
 
     @Override
     public void complete(String benchmark, boolean valid) {
-	System.out.println("[DaCapo] Completed iteration.");
+	System.out.println("[DaCapo] Completed iteration nr.: " + iterationCount);
 	super.complete(benchmark, valid);
     }
 
@@ -27,21 +37,29 @@ public class EvalCallback extends Callback {
 
     @Override
     public void start(String benchmark) {
+	startTime = System.currentTimeMillis();
 	System.out.println("[DaCapo] Starting iteration " + ++iterationCount);
 	super.start(benchmark);
     }
 
     @Override
     public void stop() {
-	System.out.println("[DaCapo] Stopping...");
+	long elapsedTime = System.currentTimeMillis() - startTime;
+	sse.getCurrentInvocation().addMeasurement(elapsedTime);
+	System.out.println("[DaCapo] Stopping... time: " + elapsedTime);
 	super.stop();
     }
 
     @Override
     public boolean runAgain() {
-	final int iterationsLeft = maxIterations - iterationCount;
-	System.out.println("[DaCapo] " + (maxIterations - iterationCount) + " iteration to go");
-	return iterationsLeft > 0;
+	evaluationData.storeEvaluationData();
+	if (sse.getCurrentInvocation().isThresholdReached()) {
+	    sse.closeCurrentInvocation();
+	    evaluationData.storeEvaluationData();
+	    System.out.println("[prm4jeval] Threshold reached after " + iterationCount + "iterations , exiting!");
+	    System.exit(1);
+	}
+	return true;
     }
 
 }
---
+++ src/main/java/prm4jeval/SteadyStateEvaluation.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4jeval;
+
+import java.io.Serializable;
+
+/**
+ * Stores the current invocation and the confidence interval for previous invocations.
+ */
+public class SteadyStateEvaluation implements Serializable {
+
+    private static final long serialVersionUID = -3377899252960634603L;
+
+    private SteadyStateInvocation steadyStateInvocation;
+    private final ConfidenceInterval confidenceInterval;
+
+    public SteadyStateEvaluation() {
+	confidenceInterval = new ConfidenceInterval(0.99D);
+	steadyStateInvocation = new SteadyStateInvocation();
+    }
+
+    public SteadyStateInvocation getCurrentInvocation() {
+	return steadyStateInvocation;
+    }
+
+    public void closeCurrentInvocation() {
+	confidenceInterval.addMeasurement(steadyStateInvocation.getMean());
+	steadyStateInvocation = new SteadyStateInvocation();
+    }
+
+}
