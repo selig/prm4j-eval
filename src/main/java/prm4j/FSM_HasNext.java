19793f6761375490b46c580aeaf75f1a96ae60b1
Mateusz Parzonka <parzonka@gmail.com>
2013-01-21 01:57:08 +0100

    Add FSMs HasNext and SafeSync{Map, Collection}

---
+++ src/main/java/prm4j/FSM_HasNext.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j;
+
+import java.util.Iterator;
+
+import prm4j.api.Alphabet;
+import prm4j.api.MatchHandler;
+import prm4j.api.MatchHandler.MatchCounter;
+import prm4j.api.Parameter;
+import prm4j.api.Symbol1;
+import prm4j.api.fsm.FSM;
+import prm4j.api.fsm.FSMState;
+
+@SuppressWarnings("rawtypes")
+public class FSM_HasNext {
+
+	public final Alphabet alphabet = new Alphabet();
+
+	public final Parameter<Iterator> i = alphabet.createParameter("i", Iterator.class);
+
+	public final Symbol1<Iterator> hasNext = alphabet.createSymbol1("hasNext", i);
+	public final Symbol1<Iterator> next = alphabet.createSymbol1("next", i);
+
+	public final FSM fsm = new FSM(alphabet);
+
+	public final  MatchCounter matchCounter = MatchHandler.MATCH_COUNTER;
+
+	public final FSMState initial = fsm.createInitialState();
+	public final FSMState safe = fsm.createState();
+	public final FSMState error = fsm.createAcceptingState(matchCounter);
+
+	public FSM_HasNext() {
+	    initial.addTransition(hasNext, safe);
+	    initial.addTransition(next, error);
+	    safe.addTransition(hasNext, safe);
+	    safe.addTransition(next, initial);
+	    error.addTransition(next, error);
+	    error.addTransition(hasNext, safe);
+	}
+}
---
+++ src/main/java/prm4j/FSM_SafeSyncCollection.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2012 Mateusz Parzonka, Eric Bodden
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Mateusz Parzonka - initial API and implementation
+ */
+package prm4j;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import prm4j.api.Alphabet;
+import prm4j.api.MatchHandler;
+import prm4j.api.MatchHandler.MatchCounter;
+import prm4j.api.Parameter;
+import prm4j.api.Symbol1;
+import prm4j.api.Symbol2;
+import prm4j.api.fsm.FSM;
+import prm4j.api.fsm.FSMState;
+
+@SuppressWarnings("rawtypes")
+public class FSM_SafeSyncCollection {
+
+    public final Alphabet alphabet = new Alphabet();
+
+    public final Parameter<Collection> c = alphabet.createParameter("c", Collection.class);
+    public final Parameter<Iterator> i = alphabet.createParameter("i", Iterator.class);
+
+    public final Symbol1<Collection> sync = alphabet.createSymbol1("sync", c);
+    public final Symbol2<Collection, Iterator> asyncCreateIter = alphabet.createSymbol2("asyncCreateIter", c, i);
+    public final Symbol2<Collection, Iterator> syncCreateIter = alphabet.createSymbol2("syncCreateIter", c, i);
+    public final Symbol1<Iterator> accessIter = alphabet.createSymbol1("accessIter", i);
+
+    public final FSM fsm = new FSM(alphabet);
+
+    public final MatchCounter matchCounter = MatchHandler.MATCH_COUNTER;
+
+    public final FSMState initial = fsm.createInitialState();
+    public final FSMState s1 = fsm.createState();
+    public final FSMState s2 = fsm.createState();
+    public final FSMState error = fsm.createAcceptingState(matchCounter);
+
+    public FSM_SafeSyncCollection() {
+	initial.addTransition(sync, s1);
+	s1.addTransition(asyncCreateIter, error);
+	s1.addTransition(syncCreateIter, s2);
+	s2.addTransition(accessIter, error);
+    }
+
+}
