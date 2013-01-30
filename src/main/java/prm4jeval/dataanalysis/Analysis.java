0ee3558e9d4e4572042b1fd9da8b37d60144264a
Mateusz Parzonka <parzonka@gmail.com>
2013-01-30 17:32:48 +0100

    Improve Analysis

--- src/main/java/prm4jeval/dataanalysis/Analysis.java
+++ src/main/java/prm4jeval/dataanalysis/Analysis.java
@@ -20,42 +20,31 @@ public class Analysis {
 
     private final static String OUTPUT_PATH = "data-analysis";
 
+    private final static TableParser1 BASELINE = new TableParser1("logs/final/baseline.log") {
+	@Override
+	public void parseLine(String line) {
+	    final String[] s = line.split(" ");
+	    if (s[3].equals("mean")) {
+		put(s[1], Double.parseDouble(s[5]) / 1000);
+	    }
+	}
+    };
+
     public static void main(String[] args) {
 
-	// => output normalized performance measures for all properties
-	// - 0 -- 1 -- ---2 ----- 3 -- 4 ------ 5 --------- 6
-	// [05, xalan, HasNext, mean, 08, 4456.400000, 0.017796]
+	// write benchmark means
+	CITableWriter.writeCITable(BASELINE, OUTPUT_PATH, "");
 
-	CITableWriter.writeCITable( //
-		new TableParser2("logs/javamop.log") {
-		    @Override
-		    public void parseLine(String line) {
-			String[] s = line.split(" ");
-			if (s[3].equals("mean")) {
-			    put(s[2], s[1], Double.parseDouble(s[5]) / 1000);
-			}
-		    }
-		}, new TableParser1("logs/baseline.log") {
-		    @Override
-		    public void parseLine(String line) {
-			String[] s = line.split(" ");
-			if (s[3].equals("mean")) {
-			    put(s[1], Double.parseDouble(s[5]) / 1000);
-			}
-		    }
-		}, OUTPUT_PATH, "");
-
-	// JAVAMOP => output normalized memory measures for all properties
-	// - 0 -- 1 -- ---2 -------------- 3 ---- 4 ------- 5 ------ 6
-	// - 0 -- 1 -- ---2 -------------- 3 --- mean ---- max -- events
-	// [08 jython SafeSyncCollection iter 74.947705 88.861328 438394]
+	// write normalized performance values for all approaches
+	writeNormalizedPerformanceTable("logs/final/javamop");
+	writeNormalizedPerformanceTable("logs/final/prm4j");
 
 	final Set<String> skippedFirstLines = new HashSet<String>();
 	CITableWriter.writeCITable( //
-		new TableParser2("logs/javamop.log.mem.log") {
+		new TableParser2("logs/final/javamop.log.mem.log") {
 		    @Override
 		    public void parseLine(String line) {
-			String[] split = line.split(" ");
+			final String[] split = line.split(" ");
 			// the first measurement of each invocation will not be counted (warm-up)
 			if (!skippedFirstLines.add(split[0] + " " + split[1] + " " + split[2])) {
 			    put(split[2], split[1], Double.parseDouble(split[4]));
@@ -64,10 +53,10 @@ public class Analysis {
 		}, OUTPUT_PATH, "mean");
 	skippedFirstLines.clear();
 	CITableWriter.writeCITable( //
-		new TableParser2("logs/javamop.log.mem.log") {
+		new TableParser2("logs/final/javamop.log.mem.log") {
 		    @Override
 		    public void parseLine(String line) {
-			String[] split = line.split(" ");
+			final String[] split = line.split(" ");
 			// the first measurement of each invocation will not be counted (warm-up)
 			if (!skippedFirstLines.add(split[0] + " " + split[1] + " " + split[2])) {
 			    put(split[2], split[1], Double.parseDouble(split[5]));
@@ -75,11 +64,54 @@ public class Analysis {
 		    }
 		}, OUTPUT_PATH, "max");
 
+	writeStatsTable("logs/final/prm4j-stats", "matches", "MATCHES", 5);
+	writeStatsTable("logs/final/prm4j-stats", "memory-mean", "MEMORY", 5);
+	writeStatsTable("logs/final/prm4j-stats", "memory-max", "MEMORY", 6);
+	writeStatsTable("logs/final/prm4j-stats", "monitors-created", "MONITORS", 5);
+	writeStatsTable("logs/final/prm4j-stats", "monitors-updated", "MONITORS", 6);
+	writeStatsTable("logs/final/prm4j-stats", "monitors-ophaned", "MONITORS", 7);
+	writeStatsTable("logs/final/prm4j-stats", "monitors-collected", "MONITORS", 8);
+	writeStatsTable("logs/final/prm4j-stats", "bindings-created", "BINDINGS", 5);
+	writeStatsTable("logs/final/prm4j-stats", "bindings-collected", "BINDINGS", 6);
+	writeStatsTable("logs/final/prm4j-stats", "bindings-stored", "BINDINGS", 7);
+	writeStatsTable("logs/final/prm4j-stats", "events", "EVENTS", 5);
+
+    }
+
+    private static void writeStatsTable(final String logName, final String tableName, final String rowFilter,
+	    final int columnNr) {
+	final Set<String> skippedFirstLines = new HashSet<String>();
+	CITableWriter.writeCITable( //
+		new TableParser2(logName + ".log") {
+		    @Override
+		    public void parseLine(String line) {
+			final String[] split = line.split(" ");
+			if (split[3].equals(rowFilter)) {
+			    // the first measurement of each invocation will not be counted (warm-up)
+			    if (!skippedFirstLines.add(split[0] + " " + split[1] + " " + split[2])) {
+				put(split[2], split[1], Double.parseDouble(split[columnNr]));
+			    }
+			}
+		    }
+		}, OUTPUT_PATH, tableName);
+    }
+
+    private static void writeNormalizedPerformanceTable(final String logName) {
+	CITableWriter.writeCITable( //
+		new TableParser2(logName + ".log") {
+		    @Override
+		    public void parseLine(String line) {
+			final String[] s = line.split(" ");
+			if (s[3].equals("mean")) {
+			    put(s[2], s[1], Double.parseDouble(s[5]) / 1000);
+			}
+		    }
+		}, BASELINE, OUTPUT_PATH, "");
     }
 
     public static <T> Set<T> set(T... values) {
-	Set<T> set = new HashSet<T>();
-	for (T s : values) {
+	final Set<T> set = new HashSet<T>();
+	for (final T s : values) {
 	    set.add(s);
 	}
 	return set;
